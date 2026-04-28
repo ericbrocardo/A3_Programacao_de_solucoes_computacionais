af8485ad3f6919e70cf40e6548072870cf595595
user <ericbrocardofreire@gmail.com>
2026-04-28 20:57:13 -0300

    feat: add construtores na classe Movimentação

--- src/main/java/modelo/Movimentação.java
+++ src/main/java/modelo/Movimentação.java
@@ -4,10 +4,27 @@ import java.time.LocalDate;
 
 public class Movimentacao {
 // (Opções Fixas)
-    public enum Tipo { ENTRADA, SAIDA }
+
+    public enum Tipo {
+        ENTRADA, SAIDA
+    }
 // ATRIBUTOS
     private int id;
-    private Produto   produto;
+    private Produto produto;
     private LocalDate data;
-    private double    quantidade;
-    private Tipo      tipo;
+    private double quantidade;
+    private Tipo tipo;
+
+    //CONSTRUTOR
+    public Movimentacao() {
+    }
+
+    public Movimentacao(int id, Produto produto, LocalDate data,
+            double quantidade, Tipo tipo) {
+        this.id = id;
+        this.produto = produto;
+        this.data = data;
+        this.quantidade = quantidade;
+        this.tipo = tipo
+    
+;
