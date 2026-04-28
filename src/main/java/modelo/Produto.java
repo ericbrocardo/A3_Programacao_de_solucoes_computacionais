dbea07e80d6bbe9e167056f532df880da0e265e9
user <ericbrocardofreire@gmail.com>
2026-04-28 20:26:49 -0300

    refactor: redefinir atributos da classe Produto

---
+++ src/main/java/modelo/Produto.java
@@ -0,0 +1,12 @@
+package modelo;
+
+public class Produto {
+// ATRIBUTOS
+    private int id;
+    private String nome;
+    private double preco;
+    private String unidade;
+    private double qtdEstoque;
+    private double qtdMinima;
+    private double qtdMaxima;
+    private Categoria categoria;
