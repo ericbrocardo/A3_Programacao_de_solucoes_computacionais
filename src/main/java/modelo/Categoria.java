1f9471003d727844a6b5fc9c6e6ffdf72865344e
user <ericbrocardofreire@gmail.com>
2026-04-28 20:44:58 -0300

    feat: adicionar construtores na classe Categoria

--- src/main/java/modelo/Categoria.java
+++ src/main/java/modelo/Categoria.java
@@ -2,6 +2,7 @@ package modelo;
 
 public class Categoria {
 // (Opções Fixas)
+
     public enum Tamanho {
         PEQUENO, MEDIO, GRANDE
     }
@@ -14,3 +15,14 @@ public class Categoria {
     private String nome;
     private Tamanho tamanho;
     private Embalagem embalagem;
+    //CONSTRUTOR
+
+    public Categoria() {
+    }
+
+    public Categoria(int id, String nome, Tamanho tamanho, Embalagem embalagem) {
+        this.id = id;
+        this.nome = nome;
+        this.tamanho = tamanho;
+        this.embalagem = embalagem;
+    }
