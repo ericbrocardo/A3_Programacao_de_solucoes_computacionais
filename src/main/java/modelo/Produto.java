db6dd2f01eee9378dd13727ad2e88aabe31ee8b3
user <ericbrocardofreire@gmail.com>
2026-04-28 20:34:51 -0300

    feat: adicionar get e set na classe Produto

--- src/main/java/modelo/Produto.java
+++ src/main/java/modelo/Produto.java
@@ -27,3 +27,67 @@ public class Produto {
         this.qtdMaxima = qtdMaxima;
         this.categoria = categoria;
     }
+    //GET
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public double getQtdEstoque() {
+        return qtdEstoque;
+    }
+
+    public double getQtdMinima() {
+        return qtdMinima;
+    }
+
+    public double getQtdMaxima() {
+        return qtdMaxima;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+//SET
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
+    public void setQtdEstoque(double v) {
+        this.qtdEstoque = v;
+    }
+
+    public void setQtdMinima(double v) {
+        this.qtdMinima = v;
+    }
+
+    public void setQtdMaxima(double v) {
+        this.qtdMaxima = v;
+    }
+
+    public void setCategoria(Categoria c) {
+        this.categoria = c;
+    }
