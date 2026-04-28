5504057c8e484c22e3d28ee16bdfc851d4f85d7d
user <ericbrocardofreire@gmail.com>
2026-04-28 20:41:53 -0300

    feat: adicionar Atributo opções fixas

--- src/main/java/modelo/Categoria.java
+++ src/main/java/modelo/Categoria.java
@@ -1,6 +1,16 @@
 package modelo;
 
 public class Categoria {
-    
-}
+// (Opções Fixas)
+    public enum Tamanho {
+        PEQUENO, MEDIO, GRANDE
+    }
 
+    public enum Embalagem {
+        LATA, VIDRO, PLASTICO
+    }
+    // ATRIBUTOS
+    private int id;
+    private String nome;
+    private Tamanho tamanho;
+    private Embalagem embalagem;
