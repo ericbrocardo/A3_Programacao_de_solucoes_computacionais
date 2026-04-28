a2c463aea214692bc1dc57ac401556bdc04cd454
user <ericbrocardofreire@gmail.com>
2026-04-28 20:32:08 -0300

    feat: adicionar construtores) na classe Produto

--- src/main/java/modelo/Produto.java
+++ src/main/java/modelo/Produto.java
@@ -10,3 +10,20 @@ public class Produto {
     private double qtdMinima;
     private double qtdMaxima;
     private Categoria categoria;
+    
+//CONSTRUTOR
+    public Produto() {
+    }
+
+    public Produto(int id, String nome, double preco, String unidade,
+            double qtdEstoque, double qtdMinima, double qtdMaxima,
+            Categoria categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+        this.unidade = unidade;
+        this.qtdEstoque = qtdEstoque;
+        this.qtdMinima = qtdMinima;
+        this.qtdMaxima = qtdMaxima;
+        this.categoria = categoria;
+    }
