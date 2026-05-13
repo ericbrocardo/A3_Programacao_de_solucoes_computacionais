96388d8f9de94d8d019af50cd99db422dad135db
Nathan-Pierre <nathanpierredasilva@gmail.com>
2026-05-13 14:41:44 -0300

    ui: define painel principal e estilização do cabeçalho

--- src/main/java/visao/FrmMenuPrincipal.java
+++ src/main/java/visao/FrmMenuPrincipal.java
@@ -14,5 +14,18 @@ public class FrmMenuPrincipal extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(420, 400);
         setResizable(false);
+        
+        //Painel Principal
+        JPanel pnlPrincipal = new JPanel(new BorderLayout());
+        pnlPrincipal.setBackground(new Color(44, 62, 80));
+
+        //Cabeçalho
+        JLabel lblTitulo = new JLabel("📦 Controle de Estoque", SwingConstants.CENTER);
+        lblTitulo.setFont(new Font("Segoe UI", Font.BOLD, 20));
+        lblTitulo.setForeground(Color.WHITE);
+        lblTitulo.setBorder(BorderFactory.createEmptyBorder(24, 0, 24, 0));
+        pnlPrincipal.add(lblTitulo, BorderLayout.NORTH);
+        
+        add(pnlPrincipal);
     }
 }
