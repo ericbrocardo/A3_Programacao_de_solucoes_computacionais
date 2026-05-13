41da4f1d658c00c159585d0358bf6a50523c35a4
Arthur Coelho <136493983+arthurcoelhooo@users.noreply.github.com>
2026-05-13 19:12:31 -0300

    feat: adiciona estrutura base da tela de reajuste de precos

--- src/main/java/visao/FrmReajuste.java
+++ src/main/java/visao/FrmReajuste.java
@@ -1,13 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package visao;
 
-/**
- *
- * @author arthurcoelho
- */
-public class FrmReajuste {
-    
+import dao.ProdutoDAO;
+
+import javax.swing.*;
+import java.awt.*;
+import java.sql.SQLException;
+
+public class FrmReajuste extends JFrame {
+
+    private JTextField txtPercentual;
+    private JLabel lblResultado;
+
+    private final ProdutoDAO produtoDAO = new ProdutoDAO();
+
+    public FrmReajuste() {
+        initComponents();
+    }
 }
