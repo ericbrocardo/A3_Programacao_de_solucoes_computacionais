a66d38ef65ebcba95c4863c9885376a23a911263
Bruno Franz Zeferino <brunofranzzeferino@gmail.com>
2026-05-21 19:34:57 -0300

    fix(visao): substitui wildcard import por imports específicos em FrmReajuste

--- src/main/java/visao/FrmReajuste.java
+++ src/main/java/visao/FrmReajuste.java
@@ -2,8 +2,21 @@ package visao;
 
 import dao.ProdutoDAO;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 public class FrmReajuste extends JFrame {
 
