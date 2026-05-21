37315a5159016ae0febc71ccea73cb30ea0a946b
Bruno Franz Zeferino <brunofranzzeferino@gmail.com>
2026-05-21 17:30:05 -0300

    fix(dao): substitui wildcard import por imports específicos em MovimentacaoDAO

--- src/main/java/dao/MovimentacaoDAO.java
+++ src/main/java/dao/MovimentacaoDAO.java
@@ -2,7 +2,12 @@ package dao;
 
 import modelo.Movimentacao;
 import modelo.Produto;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
