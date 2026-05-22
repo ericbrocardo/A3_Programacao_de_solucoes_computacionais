1d753e25dce8b3e1589f2a6b3ea1890b467a3b0d
VINICIUS WEISWEILER <viniciusw2408@gmail.com>
2026-05-22 20:16:42 -0300

    docs(visao): adiciona documentação JavaDoc ao Mensagem
    - Documenta a classe Mensagem com descrição geral
    - Adiciona @param em todos os métodos estáticos
    - Documenta @return do método confirmar() com descrição do retorno booleano

--- src/main/java/visao/Mensagem.java
+++ src/main/java/visao/Mensagem.java
@@ -2,23 +2,49 @@ package visao;
 
 import javax.swing.JOptionPane;
 
+/**
+ * Classe utilitária para exibição de mensagens padronizadas ao usuário.
+ * Fornece métodos estáticos para exibir diálogos de informação,
+ * erro, aviso e confirmação.
+ */
 public class Mensagem {
 
+    /**
+     * Exibe uma mensagem de informação ao usuário.
+     *
+     * @param msg Mensagem a ser exibida
+     */
     public static void info(String msg) {
         JOptionPane.showMessageDialog(null, msg,
                 "Informação", JOptionPane.INFORMATION_MESSAGE);
     }
 
+    /**
+     * Exibe uma mensagem de erro ao usuário.
+     *
+     * @param msg Mensagem de erro a ser exibida
+     */
     public static void erro(String msg) {
         JOptionPane.showMessageDialog(null, msg,
                 "Erro", JOptionPane.ERROR_MESSAGE);
     }
 
+    /**
+     * Exibe uma mensagem de aviso ao usuário.
+     *
+     * @param msg Mensagem de aviso a ser exibida
+     */
     public static void aviso(String msg) {
         JOptionPane.showMessageDialog(null, msg,
                 "Aviso", JOptionPane.WARNING_MESSAGE);
     }
 
+    /**
+     * Exibe uma caixa de diálogo de confirmação ao usuário.
+     *
+     * @param msg Mensagem de confirmação a ser exibida
+     * @return true se o usuário confirmar, false caso contrário
+     */
     public static boolean confirmar(String msg) {
         int r = JOptionPane.showConfirmDialog(null, msg,
                 "Confirmação", JOptionPane.YES_NO_OPTION);
